56c9097b75dca4bf750215cd20e24c44662443d9
zemfira1 <craftyzema@gmail.com>
2023-10-23 11:12:23 -0400

    +

--- controllers/auth/register-controller.js
+++ controllers/auth/register-controller.js
@@ -2,9 +2,28 @@ const { User } = require("../../models/mongoosSchemas");
 const { HttpError } = require("../../helpers");
 const { ctrlWrapper } = require("../../decorators");
 const bcrypt = require("bcryptjs");
+//const { fs } = require("fs");
+const path = require("path");
+const gravatar = require("gravatar");
+
+const avatarPath = path.resolve("public", "avatars");
+console.log(avatarPath);
 
 const register = async (req, res) => {
+  // const { path: oldPath, filename } = req.file;
+  // const newPath = path.join(avatarPath, filename);
+
+  // console.log(oldPath);
+  // console.log(filename);
+  // console.log(newPath);
+
+  // await fs.rename(oldPath, newPath);
+
+  const { filename } = req.file;
   const { email, password } = req.body;
+
+  const avatar = path.join("avatars", filename);
+
   const user = await User.findOne({ email });
   if (user) {
     throw HttpError(409, `${email} in use`);
@@ -12,7 +31,15 @@ const register = async (req, res) => {
 
   const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ ...req.body, password: hashPassword });
+  const avatarURL = gravatar.url(avatar, {
+    protocol: "http",
+  });
+
+  const newUser = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   console.log(req.body);
   console.log(req.file);
@@ -20,6 +47,7 @@ const register = async (req, res) => {
   res.status(201).json({
     email: newUser.email,
     subscription: newUser.subscription,
+    avatarURL,
   });
 };
 
