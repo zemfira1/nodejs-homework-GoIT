694aa1f0ca33cc775d05ca09979ca4f29ee092ba
zemfira1 <craftyzema@gmail.com>
2023-10-26 11:35:50 -0400

    +

--- controllers/auth/avatars-controller.js
+++ controllers/auth/avatars-controller.js
@@ -1,10 +1,12 @@
 const { ctrlWrapper } = require("../../decorators");
 const { User } = require("../../models/mongoosSchemas");
 const { HttpError } = require("../../helpers");
+const Jimp = require("jimp");
+const gravatar = require("gravatar");
 
 const avatars = async (req, res) => {
-  console.log(req.file);
-  console.log(req.user);
+  //console.log(req.file);
+  //console.log(req.user);
   const { file, user } = req;
 
   if (!user) {
@@ -15,10 +17,17 @@ const avatars = async (req, res) => {
     throw HttpError(400, "File missing");
   }
 
-  await User.findByIdAndUpdate(req.user._id, { avatarURL: file.filename });
+  const image = await Jimp.read(file.path);
+  image.resize(250, 250);
+  const avatarURL = gravatar.url(user.email, {
+    protocol: "http",
+    d: "mp",
+  });
+
+  await User.findByIdAndUpdate(req.user._id, { avatarURL: avatarURL });
 
   res.status(200).json({
-    avatarURL: file.filename,
+    avatarURL,
   });
 };
 
